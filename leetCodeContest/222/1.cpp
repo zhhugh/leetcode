93f76d36fbabad4c813b25dd43ad1db5f7f7379e
zhhugh <50230762+zhhugh@users.noreply.github.com>
2021-01-03 17:07:26 +0800

    Update 1.cpp

--- leetCodeContest/222/1.cpp
+++ leetCodeContest/222/1.cpp
@@ -1,1 +1,22 @@
-#include <iotream>#include <vector>using namespace std;class Solution1 {public:    static bool cmp(vector<int>a, vector<int> b){        return a[1] > b[1];    }    int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {        sort(boxTypes.begin(), boxTypes.end(), cmp);        int ans = 0;        int len = boxTypes.size();        for(int i = 0; i < len; i++){            if(boxTypes[i][0] >= truckSize){                ans += truckSize * boxTypes[i][1];                break;            }else{                ans += boxTypes[i][0] * boxTypes[i][1];                truckSize -= boxTypes[i][0];            }        }        return ans;    }};
+class Solution1 {
+public:
+    static bool cmp(vector<int>a, vector<int> b){
+        return a[1] > b[1];
+    }
+
+    int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
+        sort(boxTypes.begin(), boxTypes.end(), cmp);
+        int ans = 0;
+        int len = boxTypes.size();
+        for(int i = 0; i < len; i++){
+            if(boxTypes[i][0] >= truckSize){
+                ans += truckSize * boxTypes[i][1];
+                break;
+            }else{
+                ans += boxTypes[i][0] * boxTypes[i][1];
+                truckSize -= boxTypes[i][0];
+            }
+        }
+        return ans;
+    }
+};
