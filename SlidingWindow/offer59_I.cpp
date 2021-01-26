66733544cda00122e334cda2d87482623dbda7f7
zhhugh <1225598165@qq.com>
2021-01-26 16:28:23 +0800

    滑动窗口的最大值

---
+++ SlidingWindow/offer59_I.cpp
@@ -0,0 +1,32 @@
+//
+// Created by zhouhan on 2021/1/26.
+//
+// 滑动窗口的最大值
+#include "../common.h"
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> qmax;
+        vector<int> ans;
+        int len = nums.size();
+        for(int i = 0; i < len; i++){
+            // 进数
+            while(!qmax.empty() && nums[qmax.back()] <= nums[i]){
+                qmax.pop_back();
+            }
+            qmax.push_back(i);
+            // 找过期下标, 如果下标过期则弹出队列， 如果非法或者没有形成大小为k的窗口则不弹
+            // 因为窗口没有出数， 也就没有下标过期
+            if(i - k == qmax.front()){
+                qmax.pop_front();
+            }
+            // 如果形成了窗口
+            if(i >= k - 1){
+                ans.push_back(nums[qmax.front()]);
+            }
+        }
+        return ans;
+    }
+};
+
