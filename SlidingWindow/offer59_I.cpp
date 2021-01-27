156cd5bbb4c5b8cebe235aa2d79da95c0e6ad300
zhhugh <1225598165@qq.com>
2021-01-27 10:47:07 +0800

    单调双端队列

--- SlidingWindow/offer59_I.cpp
+++ SlidingWindow/offer59_I.cpp
@@ -1,13 +1,13 @@
 //
 // Created by zhouhan on 2021/1/26.
 //
-// 滑动窗口的最大值
+// 滑动窗口的最大值（最小值）
 #include "../common.h"
 
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        deque<int> qmax;
+        deque<int> qmax; // 双端队列维持的性质：从左到右为严格递减
         vector<int> ans;
         int len = nums.size();
         for(int i = 0; i < len; i++){
@@ -16,12 +16,12 @@ public:
                 qmax.pop_back();
             }
             qmax.push_back(i);
-            // 找过期下标, 如果下标过期则弹出队列， 如果非法或者没有形成大小为k的窗口则不弹
+            // 找过期下标, 如果下标过期则弹出队列， 如果没有形成大小为k的窗口则不弹
             // 因为窗口没有出数， 也就没有下标过期
             if(i - k == qmax.front()){
                 qmax.pop_front();
             }
-            // 如果形成了窗口
+            // 如果形成了窗口，就记录答
             if(i >= k - 1){
                 ans.push_back(nums[qmax.front()]);
             }
